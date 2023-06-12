b289f36c9aa0689aac06b0604f563ff3d016c4f4
amy-aovenob <tawhid.w3@gmail.com>
2023-06-12 23:55:22 +0600

    m

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/signup" Component={Signup} />
+          <Route exact path="/login" Component={Login} />
+          <Route exact path="/quiz" Component={Quiz} />
+          <Route exact path="/result" Component={Result} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
